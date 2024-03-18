705bee8ae9c0952eb6e5876e12e02699a904a4d6
Thade Pfluger <thade.pfluger@tum.de>
2024-03-18 11:39:53 +0100

    18.03.24: Conflicts fixed

--- distributeSensorsOnPerimeter.m
+++ distributeSensorsOnPerimeter.m
@@ -1,44 +1,47 @@
-function SensorsPosition = distributeSensorsOnPerimeter(numSensors, rect)
+function SensorsPosition = distributeSensorsOnPerimeter(numSensors, rectWidth, rectHeight, PlotTag)
     % numSensors: Number of sensors to distribute
-    % rect: Struct with
-    % Width: Width of the rectangle
-    % Height: Height of the rectangle
+    % rectWidth: Width of the rectangle
+    % rectHeight: Height of the rectangle
 
     % Divide the angular space in equal slices
     a = linspace(0, 2*pi, numSensors+1);
-    r = rect.Width + rect.Height;
-    xx = r*cos(a) + rect.Width/2;
-    yy = r*sin(a) + rect.Height/2;
-
-    % Initialize figure
-    figure(2)
-    axis equal;
-    axis([-0.2*rect.Width 1.2*rect.Width -0.2*rect.Height 1.2*rect.Height])
-    box on
-    xlabel('x','FontSize',16)
-    ylabel('y','FontSize',16)
-    title('Sensors on the wall','FontSize',14)
-
-    hold on;
+    r = rectWidth + rectHeight;
+    xx = r*cos(a) + rectWidth/2;
+    yy = r*sin(a) + rectHeight/2;
 
     % Build the room
-    xlimit = [0 rect.Width];
-    ylimit = [0  rect.Height];
+    xlimit = [0 rectWidth];
+    ylimit = [0  rectHeight];
     xbox = xlimit([1 1 2 2 1]);
     ybox = ylimit([1 2 2 1 1]);
-    rectangle('Position', [0, 0, rect.Width, rect.Height], 'LineWidth', 2);
 
-    % Build the sensors
+    % Locate the sensors
     SensorsPosition = zeros(numSensors,2);
     for i = 1:numSensors
-        x_aux = [rect.Width/2 xx(i)];
-        y_aux = [rect.Height/2 yy(i)];
+        x_aux = [rectWidth/2 xx(i)];
+        y_aux = [rectHeight/2 yy(i)];
         [xi, yi] = intersections(x_aux, y_aux, xbox, ybox,1);
         SensorsPosition(i,:) = [xi, yi]; 
-
-        plot(xi, yi, 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
     end
 
-    hold off;
-
+    % Room with sensors plot
+    if PlotTag  
+        figure;
+        axis equal;
+        axis([-0.2*rectWidth 1.2*rectWidth -0.2*rectHeight 1.2*rectHeight])
+        box on
+        xlabel('x','FontSize',16)
+        ylabel('y','FontSize',16)
+        title('Sensors on the wall','FontSize',14)
+        hold on;
+        
+        % Room
+        rectangle('Position', [0, 0, rectWidth, rectHeight], 'LineWidth', 2);
+            
+        % Sensors
+        for i = 1:numSensors
+            plot(SensorsPosition(i,1), SensorsPosition(i,2), 'ro', 'MarkerSize', 10, 'MarkerFaceColor', 'r');
+        end
+        hold off;
+    end
 end
