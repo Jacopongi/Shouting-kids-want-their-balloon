8d57bb557f580c3d003c53917efc9daa27ee116c
Thade Pfluger <thade.pfluger@tum.de>
2024-05-22 14:18:27 +0200

    20.05.24 - forgot function isInside before

---
+++ isInside.m
@@ -0,0 +1,24 @@
+function [inside,wall] = isInside(px,py,Room)
+% short function to check if a point lies inside
+% the rooms boundaries or not
+top = (py <= Room.Height-2.1);
+right = (px <= Room.Width-2.1);
+bottom = (py >= 2.1);
+left = (px >= 2.1);
+
+inside = top && right && bottom && left;
+
+if ~inside
+    if ~top
+        wall = "top";
+    elseif ~right
+        wall = "right";
+    elseif ~bottom
+        wall = "bottom";
+    elseif ~left
+        wall = "left";
+    end
+else
+    wall = "";
+end
+end
